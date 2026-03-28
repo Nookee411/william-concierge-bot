e7b6cbe73e829e7d111c66e93d748cd3320d1df8
Daniil Votintsev <daniil43russ@gmail.com>
2026-03-28 12:34:59 +0300

    pm2 update

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'concierge-bot',
-      script: './dist/src/index.js',
+      script: './src/index.js',
       instances: 1,
       exec_mode: 'fork',
 
